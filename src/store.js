7251830a980d1c50931f22aab298ec3e442f16b5
globalsmile <mohammedbadmus823@gmail.com>
2024-10-10 11:02:00 +0100

    use named import

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk'; // Use named import
 import { composeWithDevTools } from 'redux-devtools-extension';
 import authReducer from './reducers/authReducer';
 
 const rootReducer = combineReducers({
-	auth: authReducer,
+  auth: authReducer,
 });
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
