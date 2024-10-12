a8a7b32ebfb6517c2de33846c6859862398ea5b1
globalsmile <mohammedbadmus823@gmail.com>
2024-10-12 09:14:18 +0100

    remove composeWithDevTools import

--- src/store.js
+++ src/store.js
@@ -1,13 +1,12 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { thunk } from 'redux-thunk'; // Use named import
-import { composeWithDevTools } from 'redux-devtools-extension';
 import authReducer from './reducers/authReducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
 });
 
-const composeEnhancers = typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
